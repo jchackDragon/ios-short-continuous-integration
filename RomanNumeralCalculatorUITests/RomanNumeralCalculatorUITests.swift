7e4fb29a0c53109f4e35e4a3798e27786dda9f34
jchackDragon <juancarloslopez@Juans-MacBook-Pro.local>
2017-05-26 07:39:24 -0400

    Set UnitTest and UITest to always run

--- RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
+++ RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
@@ -21,6 +21,8 @@ class RomanNumeralCalculatorUITests: XCTestCase {
         XCUIApplication().launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        
+        //XCUIApplication().por
     }
     
     override func tearDown() {
