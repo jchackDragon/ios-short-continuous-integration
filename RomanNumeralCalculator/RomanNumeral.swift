a2eb915b2c40e6191af22970824f263826a8d9ec
jchackDragon <juancarloslopez@juans-macbook-pro.local>
2017-06-10 15:06:30 +0200

    Sort the arabic Values array

--- RomanNumeralCalculator/RomanNumeral.swift
+++ RomanNumeralCalculator/RomanNumeral.swift
@@ -22,6 +22,7 @@ func toRoman(_ number: Int) -> String {
 
     let romanValues = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
     let arabicValues = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    arabicValues.sorted()
 
     var romanValue = ""
     var startingValue = number
